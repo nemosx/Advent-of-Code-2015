d21595f8f0671c06cc348941c87ebc107b5d9f56
nemosx <root.michael.r@gmail.com>
2015-12-09 19:32:05 -0500

    Day 8 Part-2 Solution

--- Day8/app.js
+++ Day8/app.js
@@ -6,17 +6,30 @@ var split = require('split');
 
 var codedCharacterCount = 0;
 var processedCount = 0;
+var isPartOne = process.argv[3] === '1';
 
 fs.createReadStream('input.txt', 'utf-8')
     .pipe(split())
     .on('data', function (line) {
-        var processedLine = line.replace(/\\"/g, '"')
-            .replace(/\\x[0-9a-f]{2}/g, 'Z')
-            .replace(/\\\\/g, '\\');
+        if (isPartOne) {
+            var processedLine = line.replace(/\\"/g, '"')
+                .replace(/\\x[0-9a-f]{2}/g, 'Z')
+                .replace(/\\\\/g, '\\');
 
-        processedCount += (processedLine.length - 2);
-        codedCharacterCount += line.length;
+            processedCount += (processedLine.length - 2);
+            codedCharacterCount += line.length;
+        }
+        else {
+            var encodedLine = '\"' + (line.replace(/\"/g, '@')
+                        .replace(/\\/g, '\\\\')
+                        .replace(/@/g, '\\\"')
+                ) + '\"';
+
+            processedCount += encodedLine.length;
+            codedCharacterCount += line.length;
+        }
     })
     .on('end', function () {
+        console.log('Count different: ' + (processedCount - codedCharacterCount));
         console.log('Count difference: ' + (codedCharacterCount - processedCount));
     });
