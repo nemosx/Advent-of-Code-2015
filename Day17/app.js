d9abf3980d5832a1360af82f26c86994bdda8ac8
nemosx <root.michael.r@gmail.com>
2015-12-20 18:33:51 -0500

    additional testing and refactoring

---
+++ Day17/app.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Michael Root on 12/20/2015.
+ */
+var fs = require('fs');
+
+var containers = fs.readFileSync(__dirname + '/input.txt', 'utf-8').split('\n');
+
+
+console.log(containers);
+
+
+module.exports.solvePuzzle = function () {
+    return 0;
+};
