113ffc9309a61e8cba5a1a8edfff6e47cf42f784
nemosx <root.michael.r@gmail.com>
2017-02-22 15:11:36 -0500

    Day 19 Part 2 Solution - Received help from Will and https://www.reddit.com/r/adventofcode/comments/3xflz8/day_19_solutions/    By inverting the production rules, I was able to arrive at a solution. I need to read more

--- Day19/app.js
+++ Day19/app.js
@@ -13,36 +13,72 @@ function findSubstrings(substring, string) {
     return indices.concat(findSubstrings(substring, string.slice(index + substring.length)).map(i => i + substring.length + index));
 }
 
-module.exports.solvePuzzle = function () {
-    var startPoint;
-    var replacements = {};
-    var uniqueMolecules = new Set();
 
+function deriveUniqueMoleculesFrom(initialMolecule, replacements) {
+    const uniqueMolecules = new Set();
+    Object.keys(replacements).forEach(function (substring) {
+        findSubstrings(substring, initialMolecule).forEach(function (index) {
+            replacements[substring].forEach(function (sub) {
+                var molecule = initialMolecule.split('');
+                molecule.splice(index, substring.length, sub);
+                let result = molecule.join('');
+
+                uniqueMolecules.add(result);
+            })
+        })
+    });
+
+    return uniqueMolecules;
+}
+
+const alreadyEvaluatedMolecules = new Set();
+
+function buildMolecule(desiredMolecule, currentMolecule, replacements, stepCount) {
+    if (currentMolecule === desiredMolecule) {
+        console.log(stepCount);
+        return stepCount;
+    }
+
+    let nextMolecules = deriveUniqueMoleculesFrom(currentMolecule, replacements);
+    alreadyEvaluatedMolecules.add(currentMolecule);
+
+    let lowestResult = Number.MAX_VALUE;
+
+    Array.from(nextMolecules).forEach(molecule => {
+        if (!alreadyEvaluatedMolecules.has(molecule) && molecule.length <= currentMolecule.length) {
+
+            let currentResult = buildMolecule(desiredMolecule, molecule, replacements, stepCount + 1);
+            if (currentResult !== -1) {
+                lowestResult = currentResult < lowestResult ? currentResult : lowestResult;
+            }
+        }
+    })
+    return lowestResult;
+}
+
+module.exports.solvePuzzle = function (isPartTwo) {
+    var desiredMolecule;
+    var replacements = {};
 
     function processLine(line) {
         var matches = line.match(/(\w+) => (\w+)/);
         if (matches) {
-            var list = replacements[matches[1]] || [];
-            list.push(matches[2]);
-            replacements[matches[1]] = list;
+            var list = replacements[matches[2]] || [];
+            list.push(matches[1]);
+            replacements[matches[2]] = list;
         }
         else if (line) {
-            startPoint = line;
+            desiredMolecule = line;
         }
     }
 
     fs.readFileSync(__dirname + '/input.txt', 'utf-8').split('\n').forEach(processLine);
 
-    Object.keys(replacements).forEach(function (substring) {
-        findSubstrings(substring, startPoint).forEach(function (index) {
-            replacements[substring].forEach(function (sub) {
-                var molecule = startPoint.split('');
-                molecule.splice(index, substring.length, sub);
-                uniqueMolecules.add(molecule.join(''));
-            })
-        })
-    });
-
-    return uniqueMolecules.size;
+    if (isPartTwo) {
+        return buildMolecule('e',desiredMolecule, replacements, 0, 0);
+    }else {
+        return deriveUniqueMoleculesFrom(desiredMolecule, replacements).size;
+    }
 };
 
+console.log(module.exports.solvePuzzle(true));
